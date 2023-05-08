2b2ed4ae822202efcd3a599dcfa8df92be08862f
fnriv <fnrivera@ucsd.edu>
2023-05-07 23:35:30 -0700

    Updated init function for explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,49 +4,49 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  const synth = window.speechSynthesis;
 
-}
-const synth = window.speechSynthesis;
+  const inputTxt = document.querySelector('textarea');
+  const voiceSelect = document.querySelector("select");
+  const press = document.querySelector('button');
 
-const inputTxt = document.querySelector('textarea');
-const voiceSelect = document.querySelector("select");
-const press = document.querySelector('button');
+  let voices = [];
 
-let voices = [];
+  function populateVoiceList() {
+    voices = synth.getVoices();
 
-function populateVoiceList() {
-  voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
 
-  for (let i = 0; i < voices.length; i++) {
-    const option = document.createElement("option");
-    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
 
-    if (voices[i].default) {
-      option.textContent += " — DEFAULT";
-    }
-
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    voiceSelect.appendChild(option);
-  }
-}
-
-populateVoiceList();
-if (speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = populateVoiceList;
-}
-
-press.addEventListener('click', () => {
-  const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
-  const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
-  for (let i = 0; i < voices.length; i++) {
-    if (voices[i].name === selectedOption) {
-      utterThis.voice = voices[i];
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
     }
   }
-  utterThis.pitch = 1;
-  utterThis.rate = 1;
-  synth.speak(utterThis);
 
-  inputTxt.blur();
-});
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  press.addEventListener('click', () => {
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    utterThis.pitch = 1;
+    utterThis.rate = 1;
+    synth.speak(utterThis);
+
+    inputTxt.blur();
+  });
+
+}
